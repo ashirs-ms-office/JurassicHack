d7d24cf9c7798a44a8ce6fcce1c48c6d3c985481
ashirs-ms-office <ashirs@microsoft.com>
2015-06-23 21:28:11 -0700

    correct VS project check-in

---
+++ xl2vso/xl2vso/VSOConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xl2vso
+{
+    class VSOConfig
+    {
+        public static string  userName { get; set; }
+        public static string password { get; set; }
+        public static string accountName { get; set; }
+        public static string ProjectName { get; set; }
+    }
+}
